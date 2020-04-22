87a177e8f265cb83b3bf1c93a70593f1bd65774c
root <root@bcb25a64cf03>
2020-04-22 00:00:24 +0000

    pentagoncrazy and spiral java

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,17 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-	
+		Robot r2d2 = new Robot("mini");
+		r2d2.penDown();
+		r2d2.setSpeed(100);
 		
-		
+		r2d2.move(50);
+		r2d2.turn(90);
+		r2d2.move(50);
+		r2d2.turn(90);
+		r2d2.move(50);
+		r2d2.turn(90);
+		r2d2.move(50);
+		r2d2.turn(90);	
 	}
 }
