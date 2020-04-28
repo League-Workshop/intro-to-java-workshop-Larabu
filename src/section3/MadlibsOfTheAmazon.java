9946c2e0ff1fe232b5e96c08c242ba214117a117
root <root@decf973cd140>
2020-04-28 23:56:07 +0000

    madlibs and candycrush

--- src/section3/MadlibsOfTheAmazon.java
+++ src/section3/MadlibsOfTheAmazon.java
@@ -7,17 +7,17 @@ public class MadlibsOfTheAmazon {
 	public static void main(String[] args) {
 		// Put this sentence in a pop up:
 		// If you find yourself having to cross a piranha-infested river, here's how to do it...
-		
+		JOptionPane.showMessageDialog(null, "If you find yourself having to cross a piranha infested river, here's how to do it");
 		// Get the user to enter an adjective
-
+		String Adjective = JOptionPane.showInputDialog(null, "pick an adjective");
 		// Get the user to enter a type of liquid
-
+		String Liquid = JOptionPane.showInputDialog(null, "pick a liquid");
 		// Get the user to enter a body part
-
+		String bodyPart = JOptionPane.showInputDialog(null, "pick a body part");
 		// Get the user to enter a verb
-
+		String Verb = JOptionPane.showInputDialog(null, "choose a verb");
 		// Get the user to enter a place
-
+		String Place = JOptionPane.showInputDialog(null, "choose a setting");
 		// Fit the user's words into this sentence, and save it in a String:
 		// Piranhas are more [adjective] during the day, so cross the river at
 		// night. Piranhas are attracted to fresh [type of liquid] and will most
@@ -26,7 +26,7 @@ public class MadlibsOfTheAmazon {
 		// back to the [place]. Good luck!
 		
 		// Make a pop-up for the final story. You can use \n to go to the next line
-		
+		JOptionPane.showMessageDialog(null, "Piranhas are more " + Adjective + " during the day, so cross the river at night. Pirahnas are attracted to fresh " + Liquid + " and will most likely take a bite out of " + bodyPart + " if you " + Verb + ". Whatever you do, if you have an open wound, try to find another way to get back to " + Place + ". Good Luck!");
 
 	}
 }
